57f49c6b045221f236fc27ce6fe4c9f508d16ced
Kirill Erofeev <forlovesai@gmail.com>
2018-04-12 22:00:07 +0300

    measure

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <stdio.h>
 #include <cstdlib>
+
+#include "omp.h"
+
 #include "cppDES/des.h"
 #include "crush.hpp"
-
-const unsigned long ONE = 1;
+#include "measure.hpp"
+#include "../Data-Encryption-Standard/DES.hpp"
 
 static unsigned long x=123456789, y=362436069, z=521288629;
 
@@ -47,7 +50,7 @@ unsigned long effective_bits_equation4_13bits(unsigned long p, unsigned long c) 
                 get_nth_bit(c, 7) ^
                 get_nth_bit(c, 18) ^
                 get_nth_bit(c, 24) ^
-                get_nth_bit(c, 28)) >> 12))
+                get_nth_bit(c, 28)) >> 12));
 
     return result;
 }
@@ -65,7 +68,7 @@ unsigned long effective_bits_equation4_12bits(unsigned long k1, unsigned long k1
               (get_nth_bit(k16, 44) >> 8) &
               (get_nth_bit(k16, 45) >> 9) &
               (get_nth_bit(k16, 46) >> 10) &
-              (get_nth_bit(k16, 47) >> 11)) 
+              (get_nth_bit(k16, 47) >> 11));
 
     return result;
 }
@@ -78,63 +81,76 @@ unsigned long effective_bits_equation5_12bits(unsigned long k1, unsigned long k1
     return effective_bits_equation4_12bits(k16, k1);
 }
 
-unsigned long left_side_of_equation4(unsigned long f1, unsigned long f16, unsigned long t, unsigned long k) {
-    unsigned long result = 0;
+//unsigned long left_side_of_equation4(unsigned long f1, unsigned long f16, unsigned long t, unsigned long k) {
+//    unsigned long result = 0;
+//
+//    unsigned long result = 0;
+//    result = (get_nth_bit(t, 12) ^
+//              get_nth_bit(f1, 7) ^ get_nth_bit(f1, 18) ^ get_nth_bit(f1, 24) ^
+//              get_nth_bit(f16, 15))
+//
+//    return result;
+//}
+//
+//unsigned long left_side_of_equation5(unsigned long f1, unsigned long f16, unsigned long t, unsigned long k) {
+//    return left_side_of_equation4(f1, f16, k, t);
+//}
 
-    unsigned long result = 0;
-    result = (get_nth_bit(t, 12) ^
-              get_nth_bit(f1, 7) ^ get_nth_bit(f1, 18) ^ get_nth_bit(f1, 24) ^
-              get_nth_bit(f16, 15))
+uint64_t KEY = 0x0000000000000000;
 
-    return result;
-}
-
-unsigned long left_side_of_equation5(unsigned long f1, unsigned long f16, unsigned long t, unsigned long k) {
-    return left_side_of_equation4(f1, f16, k, t);
-}
-
-int main(){
-	int TA[8192], TB[8192];
+void run(uint64_t n){
+    int TA[8192], TB[8192];
     int KA[4096], KB[4096];
-	long count = 0;
+    long count = 0;
 
-    std::cout << (1 << 2);
+unsigned long p = 0;
+unsigned long c = 0;
 
-	for (long i = 0; i < (ONE << 43); ++i) {
+#pragma omp parallel for
+    for (long i = 0; i < n; ++i) {
         unsigned long p = fast_random();
-        unsigned long c = DES::encrypt(p);
+        unsigned long c = DES::encrypt(p, KEY);
+        //unsigned long c = Cipher(p, KEY);
 
-        TA[effective_bits_equation4_13bits(p, c)]++;
-        TB[effective_bits_equation5_13bits(p, c)]++;
-	}
-
-    for (long k = 0; k < (ONE << 12); ++k) {
-        for (long t = 0; t < (ONE << 13); ++t) {
-            if (left_side_of_equation4(DES::f(p, 1), DES::f(p, 16), t, k) == 0) {
-                KA[k] += TA[t];
-            }
-
-            if (left_side_of_equation5(DES::f(p, 1), DES::f(p, 16), t, k) == 0) {
-                KB[k] += TB[t];
-            }
-        }
+        ++(TA[effective_bits_equation4_13bits(p, c)]);
+        ++(TB[effective_bits_equation5_13bits(p, c)]);
     }
+}
+uint64_t encrypt_wrapper(uint64_t p, uint64_t key){
+    return DES::encrypt(p, key);
+}
+int main(){
 
-	std::cout << N/2/2 << std::endl;
-	uint64_t key = 0x0000;
-	uint64_t msg = 0x0000;
-	
-	std::cout << "key=" << key << std::endl;
-	std::cout << "msg=" << msg << std::endl;
-	uint64_t cipher = DES::decrypt(msg, key);
-	//std::cout << hex << cipher << std::endl;
+    //for (long k = 0; k < (ONE << 12); ++k) {
+    //    for (long t = 0; t < (ONE << 13); ++t) {
+    //        if (left_side_of_equation4(DES::f(p, 1), DES::f(p, 16), t, k) == 0) {
+    //            KA[k] += TA[t];
+    //        }
 
-	std::vector<T> r0{N/2 + 1488, N/2 + 55, N/2 - 77, N/2, N/2 + 1, N/2 - 90, N/2 + 7, N/2 - 3};
-	std::vector<T> r1{N/2 + 1488, N/2 + 55, N/2 - 77, N/2, N/2 + 1, N/2 - 90, N/2 + 7, N/2 - 3};
-	print(r1);  
-	std::cout << std::endl;
-	rearrange(r0.data(), r1.data());
-	print(r1);
+    //        if (left_side_of_equation5(DES::f(p, 1), DES::f(p, 16), t, k) == 0) {
+    //            KB[k] += TB[t];
+    //        }
+    //    }
+    //}
 
-        crush(r0.data(), r1.data());
+    ///std::cout << N/2/2 << std::endl;
+    ///uint64_t key = 0x0000;
+    ///uint64_t msg = 0x0000;
+    ///
+    ///std::cout << "key=" << key << std::endl;
+    ///std::cout << "msg=" << msg << std::endl;
+    ///uint64_t cipher = DES::decrypt(msg, key);
+    /////std::cout << hex << cipher << std::endl;
+
+    ///std::vector<T> r0{N/2 + 1488, N/2 + 55, N/2 - 77, N/2, N/2 + 1, N/2 - 90, N/2 + 7, N/2 - 3};
+    ///std::vector<T> r1{N/2 + 1488, N/2 + 55, N/2 - 77, N/2, N/2 + 1, N/2 - 90, N/2 + 7, N/2 - 3};
+    ///print(r1);  
+    ///std::cout << std::endl;
+    ///rearrange(r0.data(), r1.data());
+    ///print(r1);
+
+    ///crush(r0.data(), r1.data());
+
+    std::cout << measure<>::execution(encrypt_wrapper, (uint64_t)0, (uint64_t)0) << std::endl;
+    std::cout << measure<>::execution(run, ONE << 23) << std::endl;
 }
